3eefdc5eb718db299e85ed189add2fe671c165a1
Heoroth <rusiecki.romain@gmail.com>
2015-11-19 19:51:20 +0100

    Update interfaces.h
    ajout des elements pour l'affichage des lettres dans le .h

--- Ruzzle/interfaces.h
+++ Ruzzle/interfaces.h
@@ -14,8 +14,9 @@ typedef struct{
 
 //Struct for screen with the play grid
 typedef struct{
-    SDL_Surface *title, *background, *faces, *trophy;
-    SDL_Rect logoRuzzlePosition, backgroundPosition, trophyPosition, clip[5];
+    SDL_Surface *title, *background, *faces, *trophy, *letter;
+    SDL_Rect logoRuzzlePosition, backgroundPosition, trophyPosition, clip[5], letterPosition;
+    TTF_Font *fontLetter;
 }GridWindow;
 
 
@@ -30,6 +31,7 @@ extern GridWindow* grid_window_create();
 extern void grid_window_draw(GridWindow* grid, SDL_Surface* screen);
 extern void grid_window_destroy(GridWindow* grid);
 extern void apply_surface( int x, int y, SDL_Surface *src, SDL_Surface* dest, SDL_Rect* clip);
+extern void letter_display(char * nameFile, GridWindow* grid, SDL_Surface* screen);
 
 //Function for init and close SDL
 extern void close();
