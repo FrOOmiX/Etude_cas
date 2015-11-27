0b394bf24e4c5e30b42e41f599667759014637fb
Heoroth <rusiecki.romain@gmail.com>
2015-11-27 18:59:37 +0100

    Update interfaces.h

--- Ruzzle/interfaces.h
+++ Ruzzle/interfaces.h
@@ -1,9 +1,9 @@
 #ifndef _INTERFACES_H
 #define _INTERFACES_H
 
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
 
 //Struct for principal screen
 typedef struct{
@@ -15,10 +15,9 @@ typedef struct{
 //Struct for screen with the play grid
 typedef struct{
     SDL_Surface *title, *background, *faces, *trophy, *letter;
-    SDL_Rect logoRuzzlePosition, backgroundPosition, trophyPosition, clip[5], letterPosition;
+    SDL_Rect logoRuzzlePosition, backgroundPosition, trophyPosition, clip[5], clipClic[5], letterPosition, pos;
     TTF_Font *fontLetter;
 }GridWindow;
-
 
 //functions usefull for PrincipalWindow
 extern PrincipalWindow* principal_window_create();
@@ -28,10 +27,11 @@ extern void principal_window_destroy(PrincipalWindow* menu);
 
 //Function usefull for GridWindow
 extern GridWindow* grid_window_create();
-extern void grid_window_draw(GridWindow* grid, SDL_Surface* screen);
+extern void grid_window_draw(GridWindow* grid, SDL_Surface* screen, SDL_Event event);
 extern void grid_window_destroy(GridWindow* grid);
-extern void apply_surface( int x, int y, SDL_Surface *src, SDL_Surface* dest, SDL_Rect* clip);
+extern void apply_surface( int x, int y, SDL_Surface *src, SDL_Surface* dest, SDL_Rect* clip, GridWindow* grid);
 extern void letter_display(char * nameFile, GridWindow* grid, SDL_Surface* screen);
+extern void grid_window_draw_on_clic(GridWindow* grid, SDL_Surface* screen, SDL_Event event);
 
 //Function for init and close SDL
 extern void close();
