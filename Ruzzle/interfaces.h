469c470a52f07dc10acd5cc13c324358e2743d4a
Heoroth <rusiecki.romain@gmail.com>
2015-12-09 21:04:50 +0100

    redefinition onClic en int

--- Ruzzle/interfaces.h
+++ Ruzzle/interfaces.h
@@ -45,7 +45,7 @@ extern void grid_window_destroy(GridWindow* grid);
 extern void apply_surface( int x, int y, SDL_Surface *src, SDL_Surface* dest, SDL_Rect* clip, GridWindow* grid);
 extern void letter_display(char * nameFile, GridWindow* grid, SDL_Surface* screen);
 extern void grid_window_draw_on_clic(GridWindow* grid, SDL_Surface* screen, SDL_Event event,Trie *t,Cell gride[N][N], int cpt);
-extern void onClic(GridWindow* grid,int x,int y, int *pointeurSurA, int *pointeurSurB,int *pointeurSurR,int *pointeurSurC, int tab[17][2]);
+extern int onClic(GridWindow* grid,int x,int y, int *pointeurSurA, int *pointeurSurB,int *pointeurSurR,int *pointeurSurC, int tab[17][2]);
 extern Uint32 timer(Uint32 interval, void* grid);
 extern int grid_window_update(GridWindow *grid);
 
