cb21efa4df6fef1cead8eff44d2ebd90b3dd38e1
Heoroth <rusiecki.romain@gmail.com>
2015-12-12 18:34:07 +0100

    Update interfaces.h

--- Ruzzle/interfaces.h
+++ Ruzzle/interfaces.h
@@ -1,9 +1,9 @@
 #ifndef _INTERFACES_H
 #define _INTERFACES_H
 
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
 #include "Grid.h"
 #include "Grid.h"
 #include "Trie.h"
@@ -20,9 +20,9 @@ typedef struct{
     SDL_TimerID timerID;
     unsigned int secondsLeft;
     int coord[17][2];
-    SDL_Surface *title, *background, *faces, *trophy, *letter, *timer;
-    SDL_Rect logoRuzzlePosition, backgroundPosition, trophyPosition, clip[5], clipClic[5], letterPosition, pos, timerPosition;
-    TTF_Font *fontLetter, *fontTimer;
+    SDL_Surface *title, *background, *faces, *trophy, *letter, *timer, *scoreDisplay;
+    SDL_Rect logoRuzzlePosition, backgroundPosition, trophyPosition, clip[5], clipClic[5], letterPosition, pos, timerPosition, scorePos;
+    TTF_Font *fontLetter, *fontTimer, *fontScore;
 }GridWindow;
 
 //Struct for screen with the score and replay grid
@@ -39,7 +39,7 @@ extern void principal_window_draw(PrincipalWindow* game_menu,SDL_Surface* screen
 extern void principal_window_destroy(PrincipalWindow* menu);
 
 //Functions usefull for GridWindow
-extern GridWindow* grid_window_create();
+extern GridWindow* grid_window_create(int *pScore, Cell gride[N][N]);
 extern void grid_window_draw(GridWindow* grid, SDL_Surface* screen, SDL_Event event);
 extern void grid_window_destroy(GridWindow* grid);
 extern void apply_surface( int x, int y, SDL_Surface *src, SDL_Surface* dest, SDL_Rect* clip, GridWindow* grid);
